245820d965ad9192c58c15fea5cc1f537db58c5c
Jimmy Svensson <jimmy888swe@gmail.com>
2021-09-19 17:39:41 +0200

    co-up with oscar

--- src/App.js
+++ src/App.js
@@ -1,55 +1,63 @@
-import './App.css';
-import { useReducer } from 'react';
+import "./App.css";
+
+import { useReducer, useEffect } from "react";
 
 function App() {
-
   let user = [
     {
-      userController: ['Orginisation']
+      userController: ["Orginisation"]
     }
-  ]
-  let  controller = ['Orginisation', 'Miljönämnd']
-  const [activeControllers, dispatch] = useReducer((activeControllers, { type, value }) => {
-    switch (type) {
-      case 'add':
-        return [...activeControllers, value];
-      case 'remove':
-        return activeControllers.filter(index => index !== value);
-      default:
-        return activeControllers;
+  ];
+  useEffect(() => {
+    if (user[0].userController.length > 0) {
+      dispatch({ type: "add", value: user[0].userController[0] });
     }
   }, []);
 
-  // toogle check/uncheck category
-  const categoryToggle = (e) => {
-    if (e.target.checked) {
-      dispatch({ type: 'add', value: e.target.value });
-    } else if (!e.target.checked) {
-      dispatch({ type: 'remove', value: e.target.value });
-    }
-  };
+  let controller = ["Orginisation", "Miljönämnd"];
+  const [activeControllers, dispatch] = useReducer(
+    (activeControllers, { type, value }) => {
+      switch (type) {
+        case "add":
+          return [...activeControllers, value];
+        case "remove":
+          return activeControllers.filter((index) => index !== value);
+        default:
+          return activeControllers;
+      }
+    },
+    []
+  );
+  console.log(activeControllers);
 
-  const handleControllerChange = (e) => { 
-    categoryToggle(e); 
+  // toogle check/uncheck category
+  const handleControllerChange = (e) => {
+    if (e.target.checked) {
+      dispatch({ type: "add", value: e.target.value });
+    } else if (!e.target.checked) {
+      dispatch({ type: "remove", value: e.target.value });
+    }
   };
 
   return (
     <div className="App">
-              {
-                controller.map((item, idx) => (
-                  <label key={idx} onChange={handleControllerChange}>
-                    <input
-                      type="checkbox"
-                      defaultChecked={user[0].userController.includes(item)}
-                      value={item}
-                    /> {item}
-                  </label>
-                ))
-              }
-              <h3>Valda</h3>
-      {activeControllers ? <p>{activeControllers.join(', ')}</p> : <p>Inget valt</p> }
+      {controller.map((item, idx) => (
+        <label key={idx} onChange={handleControllerChange}>
+          <input
+            type="checkbox"
+            defaultChecked={user[0].userController.includes(item)}
+            value={item}
+          />
+          {item}
+        </label>
+      ))}
+      <h3>Valda</h3>
+      {activeControllers.length > 0 ? (
+        <p>{activeControllers.join(", ")}</p>
+      ) : (
+        <p>Inget valt</p>
+      )}
     </div>
   );
 }
-
 export default App;
